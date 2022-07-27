dea2a392ede7ab7a397449470ba81f80a1760d26
Omar <omarfessy@gmail.com>
2022-07-27 23:51:52 +0200

    home and about page

--- flaskblog.py
+++ flaskblog.py
@@ -3,9 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return "<p>Hello, Ouou!</p>"
 
+@app.route("/about")
+def about():
+    return "<p>This is the About Page!</p>"
 
-if __name__ == '__main__':
-    app.run(debug=False)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
