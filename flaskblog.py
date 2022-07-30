4a4c0d746329035ffe56ae94c4cad30f0f274561
Omar <omarfessy@gmail.com>
2022-07-31 01:30:32 +0200

    add nav and side bar and css styles

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'Donatello', 
+        'title' : 'Blog Post 1', 
+        'content' : 'First post content', 
+        'date_posted' : '12 April 1994'
+
+    },
+
+        {
+        'author' : 'Leanardo', 
+        'title' : 'Blog Post 2', 
+        'content' : 'Second post content', 
+        'date_posted' : '13 July 1995'
+
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<p>Hello, Ouou!</p>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<p>This is the About Page!</p>"
+    var = 'Omar'
+    return render_template('about.html', title='about')
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True) 
