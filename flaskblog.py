b606d3a11d0c2e2f9cecddf3df64fa3be74c19a9
Omar <omarfessy@gmail.com>
2022-10-10 01:04:30 +0200

    create models

--- flaskblog.py
+++ flaskblog.py
@@ -1,27 +1,59 @@
 from crypt import methods
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+# you need the secret key to protect your app from crsf attacks when using forms.
 app.config['SECRET_KEY'] = '760be9e130b66f380a1d4430d42c91fa'
-posts = [
-    {
-        'author' : 'Donatello', 
-        'title' : 'I am the brain =)', 
-        'content' : 'Purle is my favorite color', 
-        'date_posted' : '12 April 1994'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/adages' #conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName";
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName";
 
-    },
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-        {
-        'author' : 'Leanardo', 
-        'title' : 'Hey Captain!', 
-        'content' : 'I love my brothers', 
-        'date_posted' : '13 July 1995'
+db = SQLAlchemy(app)
 
-    }
-]
+
+# posts = [
+#     {
+#         'author' : 'Donatello', 
+#         'title' : 'I am the brain =)', 
+#         'content' : 'Purle is my favorite color', 
+#         'date_posted' : '12 April 1994'
+
+#     },
+
+#         {
+#         'author' : 'Leanardo', 
+#         'title' : 'Hey Captain!', 
+#         'content' : 'I love my brothers', 
+#         'date_posted' : '13 July 1995'
+
+#     }
+# ]
+
+class User(db.Model):
+    __tablename__ = 'blog_user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True, nullable=True)
+    email = db.Column(db.String(120),unique=True, nullable=True)
+    image_file = db.Column(db.String(20), nullable=True, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable=True)
+    posts = db.relationship('Posts', backref='author', lazy=True)
+    def __repr__(self) -> str:
+        return f"User('{self.username}', '{self.email}','{self.password}')"
+
+class Post(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), nullable=True)
+    posted_on = db.Column(db.DateTime, nullable = False, default= datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('blog_user.id'), nullable = False)
+    def __repr__(self) -> str:
+        return f"User('{self.title}', '{self.posted_on}','{self.content}')"
 
 @app.route("/")
 @app.route("/home")
