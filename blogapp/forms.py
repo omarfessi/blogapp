23f82bf7cc55698b828418953581429fdbb5381e
Omar <omarfessy@gmail.com>
2022-10-29 18:07:11 +0200

    Enforce the registration form by custom validators to catch integrity errors (username and email must be unique)

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,6 +1,9 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+from blogapp.models import Users
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=8)])
@@ -8,6 +11,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(f'The following username : {username.data} is already taken. Please choose another one')
+    
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(f'The following email address : {email.data} is already taken. Please choose another one')
+   
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
