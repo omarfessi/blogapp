4e34700bbc42bc4fd5993cd940243d0de314527d
Omar <omarfessy@gmail.com>
2022-11-06 00:27:53 +0100

    Add possibility to change account informations

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from blogapp.models import Users
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=8)])
@@ -34,3 +35,20 @@ class LoginForm(FlaskForm):
         if not user:
             raise ValidationError(f'The following email address : {email.data} does not exist. Please log in with a valid one or register for a new account')
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=8)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Confirm changes')
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            user = Users.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(f'The following username : {username.data} is already taken. Please choose another one')
+        
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            user = Users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(f'The following email address : {email.data} is already taken. Please choose another one')
+    
--- blogapp/routes.py
+++ blogapp/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from blogapp import app, db, bcrypt
 from blogapp.models import Users, Posts
-from blogapp.forms import RegistrationForm, LoginForm
+from blogapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
 
 
 @app.route("/")
@@ -59,8 +59,20 @@ def logout():
         return(redirect(url_for('login')))
 
 
-@app.route("/account")
+@app.route("/account",  methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.username.data != current_user.username or form.email.data != current_user.email:
+            current_user.username=form.username.data
+            current_user.email=form.email.data
+            db.session.commit()
+            flash('Your account has been updated!', 'success')
+        else : flash('No Changes detected!', 'info')
+        return redirect(url_for('account'))
+    elif request.method =='GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
     image_file = url_for('static', filename= 'profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file = image_file)
+    return render_template('account.html', title='Account', form=form, image_file = image_file)
