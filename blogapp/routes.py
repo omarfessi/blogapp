4fe8417e7fe5087cba8e1b972779d5b112499ce3
Omar <omarfessy@gmail.com>
2022-11-07 00:19:10 +0100

    Resize profil pictures with Pillow

--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from blogapp import app, db, bcrypt
@@ -64,8 +65,10 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path + '/static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
-    print(picture_path)
+    output_format = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_format)
+    i.save(picture_path)
     return picture_fn
 
 @app.route("/account",  methods=['GET', 'POST'])
